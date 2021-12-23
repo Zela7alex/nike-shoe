6b74daee7eb5b479b6afe5f53a6de80b8c0b2dff
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2021-12-23 16:22:59 -0500

    reorganized repo location

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph);
+
+
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.textcontent= "Something went wrong, please try again."
+    
+} 
